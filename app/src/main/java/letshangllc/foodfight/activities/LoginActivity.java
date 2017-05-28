3c0027bdab426b91202888d6248f08d63dadc0b9
Carl Burnham <carlburnham@Carls-MacBook-Pro.local>
2017-05-28 11:36:32 -0700

    updated tabs

--- app/src/main/java/letshangllc/foodfight/activities/LoginActivity.java
+++ app/src/main/java/letshangllc/foodfight/activities/LoginActivity.java
@@ -1,11 +1,17 @@
 package letshangllc.foodfight.activities;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -41,14 +47,45 @@ public class LoginActivity extends AppCompatActivity {
         /* Check if user is logged in */
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
+        if (user != null) {
+            login(user);
+        }
 
     }
 
     public void loginOnClick(View view){
+        String email = etEmail.getText().toString(), password = etPassword.getText().toString();
 
+        if(email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this, "Please fill out both fields", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            login(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Login failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    public void login(FirebaseUser user){
+        startActivity(new Intent(this, MainActivity.class));
     }
 
     public void registerOnClick(View view){
         startActivity(new Intent(this, RegisterActivity.class));
+        finish();
     }
 }
--- app/src/main/java/letshangllc/foodfight/activities/MainActivity.java
+++ app/src/main/java/letshangllc/foodfight/activities/MainActivity.java
@@ -1,15 +1,84 @@
 package letshangllc.foodfight.activities;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import letshangllc.foodfight.R;
+import letshangllc.foodfight.fragments.FoodFightFragment;
+import letshangllc.foodfight.fragments.ProfileFragment;
+import letshangllc.foodfight.fragments.TopFoodsFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    /* Views */
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupToolbar();
+    }
+
+    private void setupToolbar(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    /* Setup the tabs in the view pager */
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new FoodFightFragment(), "ONE");
+        adapter.addFragment(new ProfileFragment(), "TWO");
+        adapter.addFragment(new TopFoodsFragment(), "THREE");
+        viewPager.setAdapter(adapter);
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
