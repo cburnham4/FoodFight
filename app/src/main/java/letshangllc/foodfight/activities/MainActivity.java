fe695d4f6a662cf61f996e8823f735e7158e5c3c
Carl Burnham <carlburnham@Carls-MacBook-Pro.local>
2017-05-28 11:59:57 -0700

    starting camera work'

--- app/src/main/java/letshangllc/foodfight/activities/MainActivity.java
+++ app/src/main/java/letshangllc/foodfight/activities/MainActivity.java
@@ -9,6 +9,9 @@ import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -80,5 +83,35 @@ public class MainActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             return mFragmentTitleList.get(position);
         }
+    }
+
+    /*
+     * Menu
+     */
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_take_photo:
+                capturePhoto();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /*
+     * Capture a photo to upload
+     */
+    private void capturePhoto(){
+
     }
 }
