a36d04f6b5ff49c3ae5ae6028bd90b71b022569a
Carl Burnham <carlburnham@Carls-MacBook-Pro.local>
2017-05-29 11:11:12 -0700

    updated registration

--- app/src/main/java/letshangllc/foodfight/activities/RegisterActivity.java
+++ app/src/main/java/letshangllc/foodfight/activities/RegisterActivity.java
@@ -1,13 +1,17 @@
 package letshangllc.foodfight.activities;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.bumptech.glide.util.Util;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -15,6 +19,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import letshangllc.foodfight.R;
+import letshangllc.foodfight.models.Utils;
 
 public class RegisterActivity extends AppCompatActivity {
     private static final String TAG = RegisterActivity.class.getSimpleName();
@@ -28,44 +33,57 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_register);
-        setupFirebase();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        setupToolbar();
         findViews();
     }
 
-    private void setupFirebase(){
-        firebaseAuth = FirebaseAuth.getInstance();
+    private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+
 
     private void findViews(){
         etEmail = (EditText) findViewById(R.id.etEmail);
         etPassword = (EditText) findViewById(R.id.etPassword);
+        etConfirmPassword = (EditText) findViewById(R.id.etConfirmPassword);
     }
 
     public void registerOnClick(View view){
         String email = etEmail.getText().toString(), password = etPassword.getText().toString();
+        String confirmPassword = etConfirmPassword.getText().toString();
 
         if(email.isEmpty() || password.isEmpty()){
-            Toast.makeText(this, "Please fill out both fields", Toast.LENGTH_LONG).show();
+            Utils.makeToast(this, "Please fill out both fields");
+            return;
+        }
+        if(!password.equals(confirmPassword)){
+            Utils.makeToast(this, "Passwords do not match");
             return;
         }
 
+        final ProgressDialog progressDialog = ProgressDialog.show(this, "Logging in", "Please Wait...", true, false);
 
         firebaseAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "createUserWithEmail:success");
                             FirebaseUser user = firebaseAuth.getCurrentUser();
-                            Toast.makeText(RegisterActivity.this, "User Created", Toast.LENGTH_LONG).show();
+                            Utils.makeToast(RegisterActivity.this, "User Created");
                             login(user);
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(RegisterActivity.this, "Registration failed.",
-                                    Toast.LENGTH_SHORT).show();
+                            Utils.makeToast(RegisterActivity.this, "Registration failed.");
+
                         }
 
                         // ...
@@ -74,6 +92,7 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void login(FirebaseUser user){
+        startActivity(new Intent(this, MainActivity.class));
         finish();
     }
 }
