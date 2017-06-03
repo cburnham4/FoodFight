e74308c1587b89451e0a30aab802cb391abbecd5
Carl Burnham <carlburnham@Carls-MacBook-Pro.local>
2017-06-02 17:36:12 -0700

    updated fragments

--- app/src/main/java/letshangllc/foodfight/activities/MainActivity.java
+++ app/src/main/java/letshangllc/foodfight/activities/MainActivity.java
@@ -1,7 +1,11 @@
 package letshangllc.foodfight.activities;
 
+
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.IdRes;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -21,10 +25,13 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.mindorks.placeholderview.SwipeDecor;
 import com.mindorks.placeholderview.SwipePlaceHolderView;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 import java.util.ArrayList;
 
 import letshangllc.foodfight.R;
+import letshangllc.foodfight.fragments.MealSwipeFragment;
 import letshangllc.foodfight.models.DatabaseConstants;
 import letshangllc.foodfight.models.FirebaseHelper;
 import letshangllc.foodfight.models.layoutbindings.MealCard;
@@ -46,7 +53,9 @@ public class MainActivity extends AppCompatActivity {
 
 
         /* TODO: Setup tab listeners */
+        addFragment();
         setupToolbar();
+        setupBottomBar();
     }
 
     @Override
@@ -57,6 +66,28 @@ public class MainActivity extends AppCompatActivity {
         progressDialog.show();
         getData();
 
+    }
+
+    private void addFragment(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        MealSwipeFragment mealSwipeFragment = new MealSwipeFragment();
+        fragmentTransaction.add(R.id.fragmentPager, mealSwipeFragment);
+        fragmentTransaction.commit();
+    }
+
+    private void setupBottomBar(){
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                if (tabId == R.id.tab_main) {
+                    // The tab with id R.id.tab_favorites was selected,
+                    // change your content accordingly.
+                }
+            }
+        });
     }
 
     private void getData(){
